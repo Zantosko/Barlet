d76e6211b5ecbff70f38c14398ef937eea985365
Zach Antosko <zachary.antosko@gmail.com>
2021-08-11 21:55:41 -0500

    cleaned up code base

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const authorization = require('../middleware/authorization');
 const { User, Profile } = require('../models');
 
+//* Fetches user info from DB
 router.get('/', authorization, async (req, res) => {
 	const user = await User.findOne({
 		where: {
@@ -12,6 +13,7 @@ router.get('/', authorization, async (req, res) => {
 	res.json(user);
 });
 
+//* Fetches user profile info from DB
 router.get('/profile', authorization, async (req, res) => {
 	const user = await Profile.findOne({
 		where: {
